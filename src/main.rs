905e56bd43358b10e66f3b7a2ed8f24e48d86111
molety <molecularge@gmail.com>
2019-11-12 23:31:30 +0900

    Improved

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,7 @@ extern crate glob;
 use glob::glob;
 
 fn main() {
-    for entry in glob("**/*.*").expect("Failed to read glob pattern") {
-        match entry {
-            Ok(path) => println!("{}", path.display()),
-            Err(e) => println!("{:?}", e),
-        }
+    for path in glob("**/*.*").unwrap().filter_map(Result::ok) {
+        println!("{}", path.display());
     }
 }
