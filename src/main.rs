8bd09e43e8344f5d4f59f04239cc9b93f6319113
molety <molecularge@gmail.com>
2019-12-02 12:53:36 +0900

    Added file read process

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
 extern crate glob;
 use glob::glob;
 
-fn main() {
+fn main() -> std::io::Result<()> {
     for path in glob("**/*.*").unwrap().filter_map(Result::ok) {
         println!("{}", path.display());
+        let file = File::open(path)?;
+        let mut reader = BufReader::new(file);
+        let mut buf = String::new();
+        reader.read_to_string(&mut buf)?;
+        println!("{}", buf);
     }
+    Ok(())
 }
